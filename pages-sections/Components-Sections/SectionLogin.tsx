da00aea50071e3cd2f8955dd770a4fcb26ed0271
Kaustubh Kumar Singh <93437624+singh-kaustubh@users.noreply.github.com>
2023-05-10 22:56:35 +0530

    main 2

--- pages-sections/Components-Sections/SectionLogin.tsx
+++ pages-sections/Components-Sections/SectionLogin.tsx
@@ -13,22 +13,36 @@ import styles from "../../styles/jss/nextjs-material-kit/pages/componentsSection
 
 const useStyles = makeStyles(styles);
 export default function SectionLogin() {
-  // const [density, setDensity] = useState<number>(0);
-  // const [temperature, setTemperature] = useState<number>(0);
-  // const [pressure, setPressure] = useState<number>(0);
-  // const [specificHeat, setSpecificHeat] = useState<number>(0);
-  // const [divergentLength, setDivergentLength] = useState<number>(0);
-  // const [particleSize, setParticleSize] = useState<number>(0);
-  // const [thermalConductivity, setThermalConductivity] = useState<number>(0);
-  // const data = {
-  //   density: density,
-  //   temperature: temperature,
-  //   pressure: pressure,
-  //   specificHeat: specificHeat,
-  //   divergentLength: divergentLength,
-  //   particleSize: particleSize,
-  //   thermalConductivity: thermalConductivity,
-  // };
+  const [data, setData] = useState<object>({
+    density: 0,
+    temperature: 0,
+    pressure: 0,
+    specificHeat: 0,
+    divergentLength: 0,
+    particleSize: 0,
+    thermalConductivity: 0,
+  });
+  const [finalOutput, setFinalOutput] = useState<object>({
+    outVelocity: 0,
+    outTemperature: 0,
+  });
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    window.alert(
+      `The predicted values are Velocity:${finalOutput.outVelocity} and Temperature:${finalOutput.outTemperature}`
+    );
+    // const res = await fetch("....");
+    // const out = await res.json();
+    // setFinalOutput({
+    //   outVelocity: out.velocity,
+    //   outTemperature: out.temperature,
+    // });
+    console.log("first", 2);
+  };
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
   const classes = useStyles();
   return (
     <div className={classes.section}>
@@ -36,7 +50,11 @@ export default function SectionLogin() {
         <GridContainer justify="center">
           <GridItem xs={12} sm={6} md={4}>
             <Card>
-              <form className={classes.form}>
+              <form
+                className={classes.form}
+                onChange={onChange}
+                onSubmit={handleSubmit}
+              >
                 <div>
                   <img src="/img/coeposter.jpg" className={classes.poster} />
                 </div>
@@ -52,83 +70,85 @@ export default function SectionLogin() {
                       inputProps={{
                         type: "number",
                         autoComplete: "off",
+                        value: { data },
                       }}
                     />
                     <CustomInput
                       labelText="Specific Heat (J/kg.K)"
-                      id="second"
+                      id="specificHeat"
                       formControlProps={{
                         fullWidth: true,
                       }}
                       inputProps={{
                         type: "number",
                         autoComplete: "off",
+                        value: { data },
                       }}
                     />
                     <CustomInput
                       labelText="Thermal Conductivity  (W/m.K)"
-                      id="third"
+                      id="thermalConductivity"
                       formControlProps={{
                         fullWidth: true,
                       }}
                       inputProps={{
                         type: "number",
                         autoComplete: "off",
+                        value: { data },
                       }}
                     />
                     <CustomInput
                       labelText="Particle Size (µm)"
-                      id="third"
+                      id="particleSize"
                       formControlProps={{
                         fullWidth: true,
                       }}
                       inputProps={{
                         type: "number",
                         autoComplete: "off",
+                        value: { data },
                       }}
                     />
                     <CustomInput
                       labelText="Particle Pressure (bar)"
-                      id="third"
+                      id="pressure"
                       formControlProps={{
                         fullWidth: true,
                       }}
                       inputProps={{
                         type: "number",
                         autoComplete: "off",
+                        value: { data },
                       }}
                     />
                     <CustomInput
                       labelText="Particle Temperature (°C)"
-                      id="third"
+                      id="temperature"
                       formControlProps={{
                         fullWidth: true,
                       }}
                       inputProps={{
                         type: "number",
                         autoComplete: "off",
+                        value: { data },
                       }}
                     />
                     <CustomInput
                       labelText="Divergent Lenght (mm)"
-                      id="third"
+                      id="divergentLength"
                       formControlProps={{
                         fullWidth: true,
                       }}
                       inputProps={{
                         type: "number",
                         autoComplete: "off",
+                        value: { data },
                       }}
                     />
                   </CardBody>
                   <CardFooter className={classes.cardFooter}>
-                    <Button
-                      simple
-                      color="primary"
-                      size="lg"
-                      // onClick={handleOnSubmit}
-                    >
-                      Calculate
+                    <Button simple color="primary" size="lg">
+                      <div onClick={handleSubmit}>Calculate</div>
                     </Button>
                   </CardFooter>
                 </div>
